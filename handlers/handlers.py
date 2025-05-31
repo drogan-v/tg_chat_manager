c91c6c44695cb58b401f1405bdc3f7bbc27a260d
drogan <artnov1kov@yandex.ru>
2025-05-31 11:42:37 +0300

    refactoring: added handlers dir

---
+++ handlers/handlers.py
@@ -0,0 +1,16 @@
+from telegram import ForceReply, Update
+from telegram.ext import ContextTypes
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from dotenv import load_dotenv
 import os
 import logging
 
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import Update
+from telegram.ext import Application, CommandHandler
+
+from handlers import handlers
 
 
 logging.basicConfig(
@@ -15,27 +17,13 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
-
 def main() -> None:
     load_dotenv()
     print(os.getenv("TOKEN"))
     app = Application.builder().token(os.getenv("TOKEN")).build()
 
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("start", handlers.start))
+    app.add_handler(CommandHandler("help", handlers.help_command))
 
     app.run_polling(allowed_updates=Update.ALL_TYPES)
 
