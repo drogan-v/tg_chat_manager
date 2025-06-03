eace2a0db651058037f9b3c8b7392a9c930bbcd6
drogan <artnov1kov@yandex.ru>
2025-06-03 12:50:05 +0300

    rollback: manager and firebase because of really raw services

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -3,21 +3,15 @@ from telegram.error import TelegramError
 from telegram.ext import ContextTypes
 from telegram.ext import CommandHandler, filters, MessageHandler
 
-from services.firebase import FirebaseService
 from services.llm import LLMService
-from .auth import BotAuthHandlers
-from .manager import ManageHandlers
 
 
 class BotHandlers:
-    def __init__(self, firebase_service: FirebaseService, llm_service: LLMService) -> None:
-        self.firebase_service = firebase_service
+    def __init__(self, llm_service: LLMService) -> None:
         self.llm_service = llm_service
-        self.bot_auth_handlers = BotAuthHandlers(firebase_service, llm_service)
-        self.manage_handlers = ManageHandlers(firebase_service)
 
     def get_handlers(self) -> list:
-        return self.bot_auth_handlers.get_handlers() + self.manage_handlers.get_handlers() + [
+        return [
             CommandHandler("help", self.help_command),
             MessageHandler(filters.TEXT & ~filters.COMMAND, self.validate)
         ]
@@ -30,10 +24,7 @@ class BotHandlers:
         """Validate the message sent by the user."""
         status, reason = self.llm_service.validate_message(update.message.text)
         if 'unsafe' in status:
-            self.firebase_service.delete_user_from_chat(update.message.from_user.id, update.message.chat_id)
             await update.message.delete()
-            await context.bot.send_message(chat_id=update.message.chat_id,
-                                           text=f'/ban @{update.message.from_user.username} {reason}')
             await context.bot.send_message(chat_id=update.message.from_user.id, text=f'Вы были забанены за сообщение: '
                                                                                      f'{update.message.text}\nПричина: {reason}')
             try:
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from telegram import Update
 from telegram.ext import Application
 
 from handlers.handlers import BotHandlers
-from services.firebase import FirebaseService
 from services.llm import LLMService
 
 logging.basicConfig(
@@ -21,14 +20,11 @@ logger = logging.getLogger(__name__)
 def main() -> None:
     load_dotenv()
 
-    firebase_service = FirebaseService('secrets/firebase.json',
-                                       os.getenv('FIREBASE_DB_URL'))
-    firebase_service.initialize()
     llm_service = LLMService()
 
     app = Application.builder().token(os.getenv("TOKEN")).build()
 
-    bot_handlers = BotHandlers(firebase_service=firebase_service, llm_service=llm_service)
+    bot_handlers = BotHandlers(llm_service=llm_service)
     for handler in bot_handlers.get_handlers():
         app.add_handler(handler)
 
